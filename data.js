0dd06c8cf7e8960dba88f470785ccc6b52df64a4
ClarityTab <callitaplan.planit@gmail.com>
2026-02-01 15:07:44 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,689 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-przedpokoj-1",
+      "name": "Przedpokój 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.024914331830778735,
+        "pitch": 0.03409132293170636,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11242162732390781,
+          "pitch": 0.43841828288089957,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        },
+        {
+          "yaw": -0.199338618888806,
+          "pitch": 0.06137847275315167,
+          "rotation": 0,
+          "target": "10-azienka"
+        },
+        {
+          "yaw": 0.21716887604758917,
+          "pitch": 0.11064575819157163,
+          "rotation": 0,
+          "target": "8-pokoj-21"
+        },
+        {
+          "yaw": 1.0356367509789024,
+          "pitch": 0.35431892304220725,
+          "rotation": 0,
+          "target": "6-pokoj-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-przedpokoj-2",
+      "name": "Przedpokój 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.6041606530112897,
+        "pitch": 0.23893369168557754,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1512716052386978,
+          "pitch": 0.3890672004839111,
+          "rotation": 0,
+          "target": "2-aneks"
+        },
+        {
+          "yaw": 1.4212262705420642,
+          "pitch": 0.14489741219061614,
+          "rotation": 0,
+          "target": "3-salon"
+        },
+        {
+          "yaw": -0.33476109222697836,
+          "pitch": 0.17859372442354626,
+          "rotation": 0,
+          "target": "10-azienka"
+        },
+        {
+          "yaw": -1.2731531250587569,
+          "pitch": 0.20864199859073906,
+          "rotation": 0,
+          "target": "11-pokoj-31"
+        },
+        {
+          "yaw": 3.0543184370821654,
+          "pitch": 0.18116012944127036,
+          "rotation": 0,
+          "target": "0-przedpokoj-1"
+        },
+        {
+          "yaw": 2.8276150474247963,
+          "pitch": 0.04545637428456928,
+          "rotation": 0,
+          "target": "6-pokoj-11"
+        },
+        {
+          "yaw": 2.295729332747662,
+          "pitch": 0.22625254000939599,
+          "rotation": 0,
+          "target": "8-pokoj-21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aneks",
+      "name": "Aneks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.016611090437198328,
+        "pitch": 0.19138604552270166,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2394518146937727,
+          "pitch": 0.22780223435856684,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        },
+        {
+          "yaw": 2.691248754117317,
+          "pitch": 0.27120450505994853,
+          "rotation": 0,
+          "target": "3-salon"
+        },
+        {
+          "yaw": 3.003891995044638,
+          "pitch": 0.0369875531043391,
+          "rotation": 0,
+          "target": "4-balkon-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8242730968182279,
+        "pitch": 0.35595665037836355,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48861236254423623,
+          "pitch": 0.21750127191942425,
+          "rotation": 0,
+          "target": "2-aneks"
+        },
+        {
+          "yaw": -0.8603458099671144,
+          "pitch": 0.14739018509832036,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        },
+        {
+          "yaw": -2.562468003670805,
+          "pitch": 0.5025312327232108,
+          "rotation": 0,
+          "target": "4-balkon-1"
+        },
+        {
+          "yaw": -2.4722425842250804,
+          "pitch": 0.20887165482253778,
+          "rotation": 0,
+          "target": "5-balkon-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-balkon-1",
+      "name": "Balkon 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.4669322945611363,
+        "pitch": 0.3546571522892492,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1054899604150652,
+          "pitch": 0.42020044581114746,
+          "rotation": 0,
+          "target": "5-balkon-2"
+        },
+        {
+          "yaw": -2.9955133433993737,
+          "pitch": 0.2240298557235434,
+          "rotation": 0,
+          "target": "3-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-balkon-2",
+      "name": "Balkon 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.025734894057325874,
+        "pitch": 0.07566572086921752,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.042296164599388675,
+          "pitch": 0.44023842112469325,
+          "rotation": 0,
+          "target": "4-balkon-1"
+        },
+        {
+          "yaw": 0.025734894057325874,
+          "pitch": 0.07566572086921752,
+          "rotation": 0,
+          "target": "3-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pokoj-11",
+      "name": "Pokój 1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.169528981118786,
+        "pitch": 0.2263863289558543,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4173137295083684,
+          "pitch": 0.28555001535869096,
+          "rotation": 0,
+          "target": "7-pokoj-12"
+        },
+        {
+          "yaw": 2.596820544733019,
+          "pitch": 0.3130349873716618,
+          "rotation": 0,
+          "target": "0-przedpokoj-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pokoj-12",
+      "name": "Pokój 1.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6938664977989593,
+          "pitch": 0.3473140007650972,
+          "rotation": 0,
+          "target": "6-pokoj-11"
+        },
+        {
+          "yaw": 1.279468033488957,
+          "pitch": 0.1611593671970084,
+          "rotation": 0,
+          "target": "0-przedpokoj-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pokoj-21",
+      "name": "Pokój 2.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.1980421173388205,
+        "pitch": 0.29583924154698593,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21098951272026945,
+          "pitch": 0.32645641217117394,
+          "rotation": 0,
+          "target": "9-pokoj-22"
+        },
+        {
+          "yaw": -2.9645274641267108,
+          "pitch": 0.2255694079962698,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pokoj-22",
+      "name": "Pokój 2.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4247512638239286,
+        "pitch": 0.35880014778244274,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08063119780847572,
+          "pitch": 0.459509056545528,
+          "rotation": 0,
+          "target": "8-pokoj-21"
+        },
+        {
+          "yaw": -0.3033018749265928,
+          "pitch": 0.12644062607716755,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.5776091485312946,
+        "pitch": 0.528624376552493,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0055300157073788,
+          "pitch": 0.2594073555064398,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pokoj-31",
+      "name": "Pokój 3.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5082626615271586,
+        "pitch": 0.25463880498549685,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12007825729158128,
+          "pitch": 0.2184550217041341,
+          "rotation": 0,
+          "target": "12-pokoj-32"
+        },
+        {
+          "yaw": 3.039416130912521,
+          "pitch": 0.22365880965829632,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pokoj-32",
+      "name": "Pokój 3.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.6050474740246798,
+        "pitch": 0.10130791166090525,
+        "fov": 1.3060506449256257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.026908164385096356,
+          "pitch": 0.38104120207078296,
+          "rotation": 0,
+          "target": "11-pokoj-31"
+        },
+        {
+          "yaw": 0.16247060578174022,
+          "pitch": 0.09160439386071495,
+          "rotation": 0,
+          "target": "1-przedpokoj-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WrSch",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
